aefce454e316738194f483c7d0cb76ddfa8f6454
977299228k-tech <977299228k@gmail.com>
2026-08-11 08:58:36 +0800

    sync: update game-mate/vite.config.js

--- game-mate/vite.config.js
+++ game-mate/vite.config.js
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: './',
   plugins: [vue()],
   build: {
+    sourcemap: false,
+    minify: 'esbuild',
     rollupOptions: {
       output: {
+        entryFileNames: 'assets/app-[hash].js',
+        chunkFileNames: 'assets/chunks/[hash].js',
+        assetFileNames: 'assets/resources/[hash][extname]',
         manualChunks(id) {
           if (!id.includes('node_modules')) return
           if (id.includes('element-plus') || id.includes('@element-plus')) return 'element-plus'
