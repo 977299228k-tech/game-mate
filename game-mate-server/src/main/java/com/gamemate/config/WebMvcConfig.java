c542209c6eabee382c4605390c086a1190d47363
banmeng <977299228k@gmail.com>
2026-08-12 15:52:02 +0800

    sync: update game-mate-server/src/main/java/com/gamemate/config/WebMvcConfig.java

--- game-mate-server/src/main/java/com/gamemate/config/WebMvcConfig.java
+++ game-mate-server/src/main/java/com/gamemate/config/WebMvcConfig.java
@@ -3,7 +3,6 @@ package com.gamemate.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,16 +14,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Value("${game-mate.file.upload-path:./uploads/}")
     private String uploadPath;
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
